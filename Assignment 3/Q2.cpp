9b9a8b69496c6f22630f515150d9772a686b646e
Neel Sonawane <neelsonawane2009@gmail.com>
2026-02-05 10:39:48 +0530

    Add function to calculate sum of digits

---
+++ Assignment 3/Q2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int sod(int n)
+{
+    if (n == 0)
+        return 0;
+
+    return (n % 10) + sod(n / 10);
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    cout << sod(N) << endl;
+    return 0;
+}

65dac9519e2bf1a652feb74bc3087cb6c9517a99
Neel Sonawane <neelsonawane2009@gmail.com>
2026-02-05 10:42:16 +0530

    Add string permutation generator in Q4.cpp
    This program takes a string input from the user, sorts it, and prints all permutations of the sorted string.

---
+++ Assignment 3/Q4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    sort(str.begin(), str.end());
+
+    cout << "All permutations of \"" << str << "\":" << endl;
+
+    do
+    {
+        cout << str << endl;
+    }
+    while (next_permutation(str.begin(), str.end()));
+
+    return 0;
+}
